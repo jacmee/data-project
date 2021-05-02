3abb9371f56726a9b6f0a2e4e8e81efda4c464fe
Jack Meeker <jackmeeker@Jacks-MacBook-Pro-2.local>
2021-05-02 16:37:51 -0700

    hi

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
 import datascience
-
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/data')
+@cross_origin()
 def data():
 	return datascience.main()
 
